80822a96604cce4b50f8bfcc4b6cd5ed5be85be7
THANETNove <novel2020manga@gmail.com>
2025-02-07 13:41:15 +0700

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>ContractorSystem</title>
+    <title>physics</title>
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
     <link rel="icon" href="{{ URL::asset('/assets/img/favicon.ico') }}" type="image/x-icon" />
 
